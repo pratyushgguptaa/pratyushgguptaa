2b78bafe023e86cb4a0749cb2f19ff12693aa541
pratyushgguptaa <pratyushgguptaa@gmail.com>
2022-02-15 20:34:00 +0530

    update secret

--- check.py
+++ check.py
@@ -1,13 +1,26 @@
 import os
-word = os.environ['WORD']
+import secrets
+# word = os.environ['WORD']
 
-next = os.environ['NEXT']
+# next = os.environ['NEXT']
 
-for i in range(5):
-    if(word[i] == next[i]):
-        print('1', end='')
-    else:
-        print('0', end='')
+# for i in range(5):
+#     if(word[i] == next[i]):
+#         print('1', end='')
+#     else:
+#         print('0', end='')
 
-print(len(word))
-print(word)
+words = []
+with open('words.txt', 'r') as f:
+    for line in f:
+        words.append(line.strip())
+
+print(len(words))
+next = words[secrets.randbelow(len(words))]
+
+f = open('secret.txt', 'w')
+f.write(next.upper())
+f.close()
+
+# print(len(word))
+# print(word)
