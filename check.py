edbd378b0734357b837df6263568cc7e6865990d
pratyushgguptaa <pratyushgguptaa@gmail.com>
2022-02-15 20:50:15 +0530

    check and tbd different

--- check.py
+++ check.py
@@ -1,26 +1,14 @@
 import os
-import secrets
-# word = os.environ['WORD']
+# import secrets
+word = os.environ['WORD']
 
-# next = os.environ['NEXT']
+next = os.environ['NEXT']
 
-# for i in range(5):
-#     if(word[i] == next[i]):
-#         print('1', end='')
-#     else:
-#         print('0', end='')
+for i in range(5):
+    if(word[i] == next[i]):
+        print('1', end='')
+    else:
+        print('0', end='')
 
-words = []
-with open('words.txt', 'r') as f:
-    for line in f:
-        words.append(line.strip())
-
-print(len(words))
-next = words[secrets.randbelow(len(words))]
-
-f = open('secret.txt', 'w')
-f.write(next.upper())
-f.close()
-
-# print(len(word))
-# print(word)
+print(len(word))
+print(word)
