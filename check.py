6c71ac7989ac6e8519866d1577efdea1e0c8f963
pratyushgguptaa <pratyushgguptaa@gmail.com>
2022-02-15 19:59:03 +0530

    string match check

--- check.py
+++ check.py
@@ -1,5 +1,13 @@
 import os
 word = os.environ['WORD']
 
+next = os.environ['NEXT']
+
+for i in range(5):
+    if(word[i] == next[i]):
+        print('1', end='')
+    else:
+        print('0', end='')
+
 print(len(word))
 print(word)
