9fa90b3632d8bcbcf89d03c633ad35dc4fbf3cd0
pratyushgguptaa <pratyushgguptaa@gmail.com>
2022-02-20 02:25:11 +0530

    remove unwanted files

--- check.py
+++
@@ -1,9 +0,0 @@
-from markdown import *
-from wordle import Wordle
-from lib.retrieve import retrieve_word
-
-game = Wordle()
-users = {}
-game = game.load_game()
-# print all values of game object
-print(game.__dict__)
